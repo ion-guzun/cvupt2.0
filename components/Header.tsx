f34c1a2b35fb679891535fb02499e256c4aab568
Ion Guzun <ionguzunupt@gmail.com>
2024-05-26 14:13:13 +0300

    mounted clerk

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,10 @@
+import { UserButton } from "@clerk/nextjs";
+
 export const Header = () => {
     return (
-      <header className="h-16 w-full border-b-2 border-slate-200 px-4">
-        Header
-      </header>
+        <header className="h-16 w-full border-b-2 border-slate-200 px-4 flex items-center justify-between">
+            <div>Header</div>
+            <UserButton/>
+        </header>
     );
-  };
+};
