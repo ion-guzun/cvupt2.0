4095e4560d9328629ee1115593e0a7df0942783b
Ion Guzun <ionguzunupt@gmail.com>
2024-06-13 01:58:57 +0300

    invalid "default" export

--- app/(root)/student/submit-assignment/page.tsx
+++ app/(root)/student/submit-assignment/page.tsx
@@ -1,34 +1,39 @@
 'use client'
-
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import Link from "next/link";
-import { Separator } from "@/components/ui/separator"
+import { Separator } from "@/components/ui/separator";
 import { uploadFiles } from "@/lib/actions/submission.actions";
 import { useRouter } from "next/navigation";
 import { toast } from "@/components/ui/use-toast";
 
-type submitAssignmentFormProps = {
-    assignmentRef: string
+interface SubmitAssignmentFormProps {
+    assignmentRef: string;
 }
 
-const submitAssignmentForm = ({}: submitAssignmentFormProps) => {
+const SubmitAssignmentForm: React.FC<SubmitAssignmentFormProps> = ({ assignmentRef }) => {
   const router = useRouter();
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    const result = await uploadFiles(formData);
+    const message = result?.message ?? "An error occurred";
+    
+    toast({
+      title: message,
+      duration: 5000
+    });
+
+    if (result?.success) {
+      router.push('/');
+    }
+  };
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-white">
       <form 
-        action={async (formData: FormData) => {
-          const result = await uploadFiles(formData);
-          const message = result?.message ?? "An error occurred";
-          toast({
-            title: message,
-            duration: 5000
-          });
-          if (result?.success) {
-            router.push('/');
-          }
-        }}
+        onSubmit={handleSubmit}
         className="flex flex-col items-center space-y-4 bg-white p-8"
       >
         <Image 
@@ -47,19 +52,17 @@ const submitAssignmentForm = ({}: submitAssignmentFormProps) => {
         </div>
         <div className="flex space-x-4">
           <Button type="submit" className="w-40 bg-gray-200 hover:bg-gray-300 text-black font-bold py-2 px-4 rounded">
-            <h1 className="text-gray-700 rounded-md">
-              Upload
-            </h1>
+            Upload
           </Button>
           <Link href='/'>
-            <Button className="w-40 bg-gray-600">Back to dashboard</Button>
+              <Button className="w-40 bg-gray-600">Back to dashboard</Button>
           </Link>
         </div>
         <Separator />
         <p className="text-xs">All the files should be placed in a zip folder before uploading.</p>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default submitAssignmentForm;
+export default SubmitAssignmentForm;
